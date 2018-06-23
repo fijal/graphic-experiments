76537bf47b64d1de7effd0a096775861912b3358
Maciej Fijalkowski <fijall@gmail.com>
2018-06-23 17:43:01 +0200

    push unity stuff

---
+++ vxgi/Assets/TextureHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureHolder : MonoBehaviour {
+
+    public RenderTexture texture;
+    public Shader shader;
+
+	// Use this for initialization
+	void Start () {
+        texture = new RenderTexture(512, 512, 16);
+        GetComponent<Camera>().targetTexture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Camera>().RenderWithShader(shader, "");	
+	}
+}
