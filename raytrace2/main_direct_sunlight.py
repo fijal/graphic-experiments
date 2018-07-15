52a79e830d4fb95300b6753d17e77033e23d754d
Armin Rigo <arigo@tunes.org>
2018-07-15 18:55:39 +0200

    Playing around

--- raytrace2/main_direct_sunlight.py
+++ raytrace2/main_direct_sunlight.py
@@ -5,7 +5,7 @@ SUN_DIRECTION = Vec3(-0.2, 1, 0.1).normalize()
 SKY_COLOR = Vec3(0.8, 0.9, 1.0)
 
 
-def color(r):
+def color2(r):
     closest_t = 1e300
     for tri in tris:
         t = tri.hit(r, 0.0001, 1000)
@@ -14,7 +14,7 @@ def color(r):
             closest_t = t
     if closest_t < 1e300:
         pt = r.point_at_parameter(closest_t)
-        r = Ray(pt, SUN_DIRECTION)
+        r = Ray(pt, SUN_DIRECTION + random_point() * 0.1)
         for tri in tris:
             t = tri.hit(r, 0.0001, 1000)
             if t > 0:
@@ -25,4 +25,5 @@ def color(r):
 
 
 if __name__ == '__main__':
-    main(color)
+    #main(color2)
+    main(lambda r: (color(r) + color2(r)) * 0.5)
